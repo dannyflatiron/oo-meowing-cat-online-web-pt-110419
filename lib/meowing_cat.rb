ee0bb5b89dd403d0d5ae618606c994138f720951
Daniel Reina <dannyflatiron@gmail.com>
2019-11-20 01:07:52 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,13 +1,10 @@
 class Cat
-  attr_accessor :name, :meow
+  attr_accessor :name
 
-  # def meow=(cat_meow)
-  #   @meow = cat_meow
-  # end
-  #
-  # def meow
-  #   @meow
-  # end
+  def meow
+    puts "meow!"
+  end
+  
 end
 
 maru = Cat.new
